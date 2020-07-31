3b099762d9daeb6899fae863e90ef8d5e09f588e
srinivasAtGH <sr_230779@yahoo.com>
2020-08-01 02:00:36 +0530

    Changes related to Register and Login

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,54 +1,70 @@
 <script>
-	import { goto, stores } from '@sapper/app';
-	import ListErrors from '../_components/ListErrors.svelte';
-	import { post } from 'utils.js';
+  import { goto, stores } from "@sapper/app";
+  import ListErrors from "../_components/ListErrors.svelte";
+  import { post } from "utils.js";
 
-	const { session } = stores();
+  const { session } = stores();
 
-	let email = '';
-	let password = '';
-	let errors = null;
+  let username = "";
+  let password = "";
+  let errors = null;
 
-	async function submit(event) {
-		const response = await post(`auth/login`, { email, password });
+  async function submit(event) {
+    const response = await post(`http://localhost:3100/login`, {
+      username: username,
+      password: password
+    });
 
-		// TODO handle network errors
-		errors = response.errors;
+    // TODO handle network errors
+    errors = response.errors;
 
-		if (response.user) {
-			$session.user = response.user;
-			goto('/');
-		}
-	}
+    // TODO handle network errors
+    if (response.errors != undefined) {
+      errors = response.errors;
+    } else {
+      goto("/login/success");
+    }
+  }
 </script>
 
 <svelte:head>
-	<title>Sign in • Learning Web</title>
+  <title>Sign in • Learning Web</title>
 </svelte:head>
 
 <div class="auth-page">
-	<div class="container page">
-		<div class="row">
-			<div class="col-md-6 offset-md-3 col-xs-12">
-				<h1 class="text-xs-center">Sign In</h1>
-				<p class="text-xs-center">
-					<a href="/register">Need an account?</a>
-				</p>
+  <div class="container page">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 col-xs-12">
+        <h1 class="text-xs-center">Sign In</h1>
+        <p class="text-xs-center">
+          <a href="/register">Need an account?</a>
+        </p>
 
-				<ListErrors {errors}/>
+        <ListErrors {errors} />
 
-				<form on:submit|preventDefault={submit}>
-					<fieldset class="form-group">
-						<input class="form-control form-control-lg" type="email" placeholder="Email" bind:value={email}>
-					</fieldset>
-					<fieldset class="form-group">
-						<input class="form-control form-control-lg" type="password" placeholder="Password" bind:value={password}>
-					</fieldset>
-					<button class="btn btn-lg btn-primary pull-xs-right" type="submit" disabled='{!email || !password}'>
-						Sign in
-					</button>
-				</form>
-			</div>
-		</div>
-	</div>
+        <form on:submit|preventDefault={submit}>
+          <fieldset class="form-group">
+            <input
+              class="form-control form-control-lg"
+              type="text"
+              placeholder="User name"
+              bind:value={username} />
+          </fieldset>
+          <fieldset class="form-group">
+            <input
+              class="form-control form-control-lg"
+              type="password"
+              placeholder="Password"
+              bind:value={password} />
+          </fieldset>
+          <button
+            class="btn btn-lg btn-primary pull-xs-right"
+            type="submit"
+            disabled={!username || !password}>
+            Sign in
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
---
+++ src/routes/register/success.svelte
@@ -0,0 +1,5 @@
+<p class="text-xs-center">
+  Hurray!, you have registered successfully.
+  <a href="/login">Login</a>
+  and explore the learning web
+</p>
