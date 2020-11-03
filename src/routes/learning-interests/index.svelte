33b85b58fce11bc3dc89f79ccf46ebf94cd33b7c
pbgurav <65648399+pbgurav@users.noreply.github.com>
2020-11-03 18:09:43 +0530

    Basic Changes to Learning Interest
    Display of Data is working Update is not working.

--- src/routes/learning-interests/index.svelte
+++ src/routes/learning-interests/index.svelte
@@ -1,15 +1,74 @@
 <script>
-    let highPriorityLearning1, highPriorityLearning2; 
-    let mediumPriorityLearning1, mediumPriorityLearning2;
-    let lowPriorityLearning;
+    import { onMount } from 'svelte';
+    import { goto, stores } from "@sapper/app";
+    import ListErrors from '../_components/ListErrors.svelte';
+    import * as api from 'api.js';
+
+    const { session } = stores();
+
+    let errors = null;
+   
+    errors = 'Messages are displayed here';
+
+let userName; 
+    let interests;
+    let learningInterest1, learningInterest2; 
+    let learningInterest3, learningInterest4;
+    let skills;
     let expertiseDomain1, expertiseDomain2, expertiseDomain3, expertiseDomain4;
-    let expertiseDesc;
+    let bio;
     let isLearner = true;
     let isGuide = true;
-    let connectColearners = true;
+    
     function submit(event) {
-        // console.log(firstName, ' firstName');
+   
+       const response =  api.put("user", {
+      username: userName,
+         need_mentoring    : isLearner,
+         available_to_mentors    : isGuide,
+        skilss    : skills,
+    interestss    : interests,
+           bio: bio
+    },
+$session.user.access_token    
+    );
+
+   
+    errors = 'After Put Call';
+
+   
     }
+
+    // Load information related to  skills and interests
+    onMount(()=> {
+        api.get(
+		"user",
+		$session.user.access_token
+		).then((user)=> {
+   userName= user.username;
+       isLearner= user.need_mentoring;
+    isGuide= user.available_to_mentor;
+ skills= user.skills;
+    interests= user.interests;
+       bio= user.bio;
+ 
+    // Todo - Split skills and interests and assignt to seperate variables
+    learningInterest1= interests;
+    learningInterest2= interests;
+    learningInterest3= interests;
+    learningInterest4= interests;
+
+  expertiseDomain1 = skills;
+   expertiseDomain2 = skills;
+    expertiseDomain3 = skills;
+        expertiseDomain4 = userName;
+      
+    
+        })
+
+	})
+
+
 </script>
 
 <style>
@@ -94,18 +153,21 @@
 </style>
 
 <svelte:head>
-	<title>Learning Interests • Learning Web</title>
+	<title>Skills Profile</title>
 </svelte:head>
 
-<div class="update-user-page">
     <div class="container page">
 		<div class="row">
+<div class="update-user-page">
             <div class="col-md-8 offset-md-2 col-xs-12">
-                <h1 class="text-xs-center">Learning Interests</h1>
+                <h1 class="text-xs-center">Learning and Sharing Profiles</h1>
                 <hr />
                 <p class="desc-text green-text">Please provide us inputs to serve you better. You can either be a learner or guide or both at the same time.</p>
                 <div class="divider-grey"></div>
-                <form on:submit|preventDefault={submit}>
+
+
+         <ListErrors {errors} />
+               <form on:submit|preventDefault={submit}>
                     <fieldset class="form-group">
                         <div class="row">
                             <div class="col-sm-10">Will you like to use this platform to educate yourself?</div>
@@ -120,19 +182,14 @@
                     <p class="desc-text grey-text">Let us know your learning interests:</p>
                     <fieldset class="form-group">
                         <div class="row">
-                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={highPriorityLearning1}></div>
-                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={highPriorityLearning2}></div>
+                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={learningInterest1}></div>
+                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={learningInterest2}></div>
                         </div>
                     </fieldset>
                     <fieldset class="form-group">
                         <div class="row">
-                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={mediumPriorityLearning1}></div>
-                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={mediumPriorityLearning2}></div>
-                        </div>
-                    </fieldset>
-                    <fieldset class="form-group">
-                        <div class="row">
-                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={lowPriorityLearning}></div>
+                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={learningInterest3}></div>
+                            <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isLearner} type="text" bind:value={learningInterest4}></div>
                         </div>
                     </fieldset>
                     <div class="divider-grey"></div>
@@ -152,17 +209,13 @@
                         <div class="row">
                             <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isGuide} type="text" bind:value={expertiseDomain1}></div>
                             <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isGuide} type="text" bind:value={expertiseDomain2}></div>
-                        </div>
-                    </fieldset>
-                    <fieldset class="form-group">
-                        <div class="row">
                             <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isGuide} type="text" bind:value={expertiseDomain3}></div>
                             <div class="col-sm-6"><input class="form-control form-control-md input-desc" disabled={!isGuide} type="text" bind:value={expertiseDomain4}></div>
                         </div>
                     </fieldset>
                     <p class="desc-text orange-text">Please write down somethine interesting that how your guidance coule be helpful for learners:</p>
                     <fieldset class="form-group">
-                        <textarea class="form-control" disabled={!isGuide} rows="5" bind:value={expertiseDesc}></textarea>
+                        <textarea class="form-control" disabled={!isGuide} rows="5" bind:value={bio}></textarea>
                     </fieldset>
                     <fieldset class="form-group">
                         <button class="btn btn-md btn-primary pull-xs-center">
@@ -170,17 +223,6 @@
                         </button>
                     </fieldset>
                     <div class="divider-grey"></div>
-                    <fieldset class="form-group">
-                        <div class="row">
-                            <div class="col-sm-10">Will you like to connect to other co-learners?</div>
-                            <div class="col-sm-2">
-                                <label class="switch">
-                                    <input type="checkbox" bind:checked={connectColearners}>
-                                    <span class="slider round"></span>
-                                </label>
-                            </div>
-                        </div>
-                    </fieldset>
                 </form>
             </div>
         </div>
